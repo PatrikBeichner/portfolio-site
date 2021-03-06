93e760b163f9022a7dc7d0eb6d4633789859bb51
Patrik Beichner <patrik.beichner@gmail.com>
2021-03-06 17:06:23 -0500

    icons

---
+++ test.js
@@ -0,0 +1,18 @@
+let num1 = 2;
+
+let num2 = 4;
+
+let sum = num1 * num2 
+return sum
+
+let num3 = 5
+
+let minus = num3 - num2
+
+return minus
+
+const quokka = {
+    isAwesome: true
+}
+
+console.log(quokka) 
